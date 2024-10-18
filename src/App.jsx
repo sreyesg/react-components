8a22c935a17bd8847c951db1526cff4d79509e70
sreyesg <santiagoreyes.g@gmail.com>
2024-10-17 18:56:15 -0700

    add Navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 // src/App.jsx
 
 import './App.css';
+import Navbar from './components/Navbar/Navbar.jsx';
 
 const App = () => {
   return (
-    <h1>Hello world!</h1>
+    <> 
+      <Navbar />  
+      <main>
+        <h1>Welcome to Hyperbo.ly</h1>
+        <h2>The future is firmly in front of us!</h2>
+        <p>If you can't explain it to a fifth-grader, our product does it.</p>
+      </main>
+    </>
   );
 };
 
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+const Navbar = () => {
+    
+    return(
+        <nav id="top-navbar">
+        <a href="/">Home</a>
+        <a href="/about-us">About Us</a>
+        <a href="/money-pit">Investment Opportunities</a>
+        <a href="/the-fine-print">Terms of Service</a>
+      </nav>
+    )
+}
+export default Navbar
